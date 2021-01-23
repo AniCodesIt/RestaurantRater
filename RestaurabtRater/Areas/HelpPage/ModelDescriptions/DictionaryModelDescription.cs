9c9df1fb13b895c0db50b3457bd9540ced6f266a
anicodesit <ani.kulka@gmail.com>
2021-01-23 12:09:33 -0500

    Corrected solution folder

--- RestaurabtRater/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace RestaurabtRater.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,32 @@
+﻿using RestaurantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private readonly RestaurantDbContext _context = new RestaurantDbContext();
+        //POST (create)
+        //api/Restaurant
+        [HttpPost]
+        public async Task <IHttpActionResult> CreateRestaurant([FromBody]Restaurant model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Store the model in the database
+                _context.Restaurants.Add(model);
+               int changeCount = await _context.SaveChangesAsync();
+                
+                return Ok("Restaurant created! ");
+            }
+            //The model is not valid, reject it
+            return BadRequest(ModelState);
+        }
+    }
+}
